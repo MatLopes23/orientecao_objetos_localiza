edb7fba7a5bb542bc1a5a8529c1a429f88dba4dc
Mateus Lopes <mateusfellipe2302@gmail.com>
2021-02-06 15:40:38 -0300

    Refatorando CarroDbService para Map

--- mateus_lopes/TresCamadas/DataBase2/CarroDbService.cs
+++ mateus_lopes/TresCamadas/DataBase2/CarroDbService.cs
@@ -9,22 +9,8 @@ namespace DataBase
         public static void Salvar(IBase iBase)
         {
             Console.WriteLine($"Estou salvando o carro");
-            var campos = iBase.GetType().GetProperties();
 
-            var sql = $"insert into {iBase.GetType().Name.ToLower()}s values (";
-            List<string> colunasDb = new List<string>();
-
-            foreach(var campo in campos)
-            {
-                var campoPersistido = campo.GetCustomAttribute<CampoPersistidoAttribute>();
-                if(campoPersistido != null)
-                {
-                    colunasDb.Add(campo.Name);
-                }
-            }
-
-            sql += string.Join(",", colunasDb.ToArray());
-            sql += ")";
+            var sql = Map.Builder(iBase);
 
             Console.WriteLine(sql);
         }
---
+++ mateus_lopes/TresCamadas/DataBase2/Map.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using System.Reflection;
+
+namespace DataBase
+{
+    public class Map
+    {
+        public static string Builder(IBase iBase)
+        {
+            var campos = iBase.GetType().GetProperties();
+
+            var sql = $"insert into {iBase.GetType().Name.ToLower()}s values (";
+            List<string> colunasDb = new List<string>();
+
+            foreach (var campo in campos)
+            {
+                var campoPersistido = campo.GetCustomAttribute<CampoPersistidoAttribute>();
+                if (campoPersistido != null)
+                {
+                    colunasDb.Add(campo.Name);
+                }
+            }
+
+            return sql;
+
+        }
+    }
+}
